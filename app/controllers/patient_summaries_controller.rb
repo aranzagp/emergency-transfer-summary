af6eace93ce216462fbd6409d0b6adc884f4fc2d
aranza <aranza@michelada.io>
2019-04-07 21:23:30 -0500

    Add view and methods in helper

--- app/controllers/patient_summaries_controller.rb
+++ app/controllers/patient_summaries_controller.rb
@@ -4,5 +4,14 @@ class PatientSummariesController < ApplicationController
   def show
     @patient = Patient.first
     @facility = Facility.first
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = SummaryPdf.new(@patient)
+        send_data pdf.render,
+          filename: "patient_summary",
+          type: 'application/pdf'
+      end
+    end
   end
 end
--- app/helpers/patient_summaries_helper.rb
+++ app/helpers/patient_summaries_helper.rb
@@ -4,4 +4,16 @@ module PatientSummariesHelper
   def sentence(array, method)
     array.map(&method).to_sentence
   end
+
+  def paragraph_1(patient)
+    I18n.t('p1', patient_age: patient.age, patient_gender:   patient.gender, facility_name: patient.facility.name, patient_admission_date: patient.admission.date, patient_admission_time: patient.admission.time, patient_admission_diagnoses: sentence(patient.admission.diagnoses, :described_code), patient_admission_symptoms: sentence(patient.admission.symptoms, :description), patient_admission_observations: sentence(patient.admission.observations, :description))
+  end
+
+  def paragraph_2(patient)
+    I18n.t('p2', patient_allegies: sentence(patient.allergies, :description), patient_chronic_conditions: sentence(patient.chronic_conditions, :described_code), patient_medications: sentence(patient.medications, :full_medication) )
+  end
+
+  def paragraph_3(patient)
+    I18n.t('p3', patient_diagnostic_procedures: sentence(patient.diagnostic_procedures, :full_description), patient_diagnoses: sentence(patient.diagnoses, :described_code), patient_treatments: sentence(patient.treatments, :full_description))
+  end
 end
